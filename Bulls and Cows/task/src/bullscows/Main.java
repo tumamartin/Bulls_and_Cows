6475a087741670b5ead5edfb26784a3bbad3163f
Martin <tuma.martin@gmail.com>
2021-11-02 11:19:58 +0100

    Initial commit

---
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -0,0 +1,16 @@
+package bullscows;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The secret code is prepared: ****.\n" +
+                "\n" +
+                "Turn 1. Answer:\n" +
+                "1234\n" +
+                "Grade: None.\n" +
+                "\n" +
+                "Turn 2. Answer:\n" +
+                "9876\n" +
+                "Grade: 4 bulls.\n" +
+                "Congrats! The secret code is 9876.");
+    }
+}
